727e01bdab1e404f84fa04007eb83afafcd0a85d
andresherco <125586508+andresherco@users.noreply.github.com>
2023-08-22 12:02:00 -0500

    Add files via upload

---
+++ app/productos/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+
+
+
+class RegistrarProductoForm(FlaskForm):
+    nombre = StringField("Nombre del producto: ")
+    precio = StringField("Precio del producto: ")
+    submit = SubmitField("Guardar")
